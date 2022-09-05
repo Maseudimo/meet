2cdc57c8464db57038d10226c2f1396fc3ddd308
Maseudimo <muhammedalmaseudi99@gmail.com>
2022-09-05 12:16:57 +0300

    3.8

---
+++ src/CitySearch.js
@@ -0,0 +1,20 @@
+// src/CitySearch.js
+
+import React, { Component } from 'react';
+
+class CitySearch extends Component {
+    render() {
+        return (
+          <div className="CitySearch">
+            <input
+              type="text"
+              className="city"
+            />
+           <ul className="suggestions">
+         </ul>
+    </div>
+        );
+      }
+     }
+
+export default CitySearch;
---
+++ src/tests/CitySearch.test.js
@@ -0,0 +1,17 @@
+// src/__tests__/CitySearch.test.js
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import CitySearch from '../CitySearch';
+
+describe('<CitySearch /> component', () => {
+  test('render text input', () => {
+    const CitySearchWrapper = shallow(<CitySearch />);
+    expect(CitySearchWrapper.find('.city')).toHaveLength(1);
+  });
+});
+
+test('renders a list of suggestions', () => {
+    const CitySearchWrapper = shallow(<CitySearch />);
+    expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
+  });
