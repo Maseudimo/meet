5c03f5d86fa3144306b788ba39a4f9afe68de9ec
Maseudimo <muhammedalmaseudi99@gmail.com>
2022-09-05 14:19:49 +0300

    4.2

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,9 +3,11 @@ import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents';
 
 describe('<App /> component', () => {
   let AppWrapper;
+
   beforeAll(() => {
     AppWrapper = shallow(<App />);
   });
@@ -16,5 +18,9 @@ describe('<App /> component', () => {
 
   test('render CitySearch', () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
+
+  test('render NumberOfEvents', () => {
+    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
   });
 });
